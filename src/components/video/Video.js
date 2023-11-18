e07d563a0e9b1f5424d1a718eafb3db1552df10a
arch <julius.klumbies@gmx.de>
2023-11-18 22:45:18 +0100

    video preview

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./Video.css";
 function Video() {
   return (
     <>
@@ -9,11 +9,23 @@ function Video() {
             src="https://wallpapers.com/images/featured-full/nature-2ygv7ssy2k0lxlzu.jpg"
             alt="preview"
             srcset=""
+            className="preview-img"
           />
         </div>
-        <div className="video-descritpion-section">
-          <div className="profile-section"></div>
-          <div className="data-section"></div>
+        <div className="video-description-section">
+          <div className="profile-section">
+            <img
+              src="https://wallpapers.com/images/featured-full/nature-2ygv7ssy2k0lxlzu.jpg"
+              alt=""
+              className="video-profile-pic"
+            />
+          </div>
+          <div className="data-section">
+            <h3 className="video-title">GPT has been unleashed</h3>
+            <h6 className="video-description">
+              Fireship · 661.557 Aufrufe · vor 4 Tagen
+            </h6>
+          </div>
         </div>
       </div>
     </>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Video from "../../components/video/Video";
+import "./Home.css";
 
 function Home() {
   return (
     <>
-      <div className="video-container">
-        <Video></Video>
+      <div className="home">
+        <div className="video-container">
+          <Video></Video>
+        </div>
       </div>
     </>
   );
