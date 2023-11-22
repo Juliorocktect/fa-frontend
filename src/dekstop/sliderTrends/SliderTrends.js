1b6932bc2415e349462b4eaa3ca763a7c9e59a6c
arch <julius.klumbies@gmx.de>
2023-11-22 18:55:07 +0100

    slider update for pc

--- src/dekstop/sliderTrends/SliderTrends.js
+++ src/dekstop/sliderTrends/SliderTrends.js
@@ -6,32 +6,49 @@ import {
 } from "@fluentui/react-icons";
 
 function SliderTrends() {
+  let images = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  let imagesLinks = [
+    "https://wallup.net/wp-content/uploads/2016/01/65540-nature-forest-river.jpg",
+    "https://i2.wp.com/techbeasts.com/wp-content/uploads/2016/01/green_mountain_nature_wallpaper_hd.jpg",
+    "https://free4kwallpapers.com/uploads/originals/2015/10/04/nature._.jpg",
+    "https://wallup.net/wp-content/uploads/2016/01/65540-nature-forest-river.jpg",
+    "https://wallup.net/wp-content/uploads/2016/01/65540-nature-forest-river.jpg",
+    "https://i2.wp.com/techbeasts.com/wp-content/uploads/2016/01/green_mountain_nature_wallpaper_hd.jpg",
+    "https://free4kwallpapers.com/uploads/originals/2015/10/04/nature._.jpg",
+    "https://wallup.net/wp-content/uploads/2016/01/65540-nature-forest-river.jpg",
+    "https://wallup.net/wp-content/uploads/2016/01/65540-nature-forest-river.jpg",
+  ];
+  let current = 1;
+  let right = 2;
+  let left = 10;
+  function next() {}
+  function setLeft(newLeft) {
+    let left = document.getElementsById("left");
+  }
   return (
     <>
       <div className="slider-trend">
+        <img
+          className="image-slider left"
+          id="left"
+          src="https://wallup.net/wp-content/uploads/2016/01/65540-nature-forest-river.jpg"
+        />
         <button className="slider-button">
           <ChevronLeft12Regular className="slider-arrow" />
         </button>
-        <div className="img-slider-container">
-          <img
-            className="image-slider"
-            id="image-slider1"
-            src="https://wallup.net/wp-content/uploads/2016/01/65540-nature-forest-river.jpg"
-          />
-          <img
-            className="image-slider left"
-            id="image-slider2"
-            src="https://free4kwallpapers.com/uploads/originals/2015/10/04/nature._.jpg"
-          />
-          <img
-            className="image-slider right"
-            id="image-slider3"
-            src="https://i2.wp.com/techbeasts.com/wp-content/uploads/2016/01/green_mountain_nature_wallpaper_hd.jpg"
-          />
-        </div>
+        <img
+          className="image-slider"
+          id="center"
+          src="https://free4kwallpapers.com/uploads/originals/2015/10/04/nature._.jpg"
+        />
         <button className="slider-button">
           <ChevronRight12Regular className="slider-arrow" />
         </button>
+        <img
+          className="image-slider right"
+          id="right"
+          src="https://i2.wp.com/techbeasts.com/wp-content/uploads/2016/01/green_mountain_nature_wallpaper_hd.jpg"
+        />
       </div>
     </>
   );
