259c03d560bd6630e902850d2f9ba2f19d5f88d4
JK <113830349+Juliorocktect@users.noreply.github.com>
2023-12-04 10:08:20 +0000

    search update function

--- src/dekstop/navbar/NavbarDesk.js
+++ src/dekstop/navbar/NavbarDesk.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./w.svg";
 import "./NavbarDesk.css";
 import {
@@ -10,17 +10,30 @@ import { useNavigate } from "react-router-dom";
 
 function NavbarDesk() {
   const navigate = useNavigate();
+  let search = "";
   return (
     <>
       <div className="desk-nav">
         <div className="nav-logo">
-          <img src={logo} alt="" className="nav-logo-svg" />
+          <img
+            src={logo}
+            alt=""
+            className="nav-logo-svg"
+            onClick={navigate("/")}
+          />
         </div>
         <div className="search-container-nav">
           <div className="search-icon-container">
             <Search12Regular className="nav-icon" />
           </div>
-          <input type="text" className="search-bar-nav-desk" />
+          <input
+            type="text"
+            className="search-bar-nav-desk"
+            onKeyUp={(e) => {
+              search = e.target.value;
+              console.log(search);
+            }}
+          />
         </div>
         <div className="icons-container-nav">
           <div className="bell-container">
